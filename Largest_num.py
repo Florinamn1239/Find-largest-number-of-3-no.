4cd05a7c2af1dd51a443f8396524f566d25b2cc2
Florina Michael <165054324+Florinamn1239@users.noreply.github.com>
2024-03-27 21:50:44 +0530

    Largest_num.py

---
+++ Largest_num.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+if a > b and a > c:
+    print(a, ": Is the largest number")
+elif b > a and b > c:
+    print(b, ": Is the largest number")
+elif c > a and c > b:
+    print(c, ": Is the largest number")
